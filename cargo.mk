5084f8db44bcffe7423d45ab896333da8a98ed67
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-03-18 18:19:26 -0400

    intermediate

--- cargo.mk
+++ cargo.mk
@@ -4,22 +4,19 @@ cargo-help:### 	cargo-help
 	@awk 'BEGIN {FS = ":.*?###"} /^[a-zA-Z_-]+:.*?###/ {printf "\033[36m%-15s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
 
 cargo-install:### 	cargo install --path .
-#@. $(HOME)/.cargo/env
-	#@cargo install --path $(PWD)
-	@cargo install --locked --path $(PWD)
+## cargo install --locked --bins nostr-cli
+	@cargo install --locked  --bins nostr-cli
 
 cargo-check:### 	cargo-check
 ## cargo c
-	@. $(HOME)/.cargo/env
 	@cargo c
 
 cargo-bench:### 	cargo-bench
 ## cargo bench
-	@. $(HOME)/.cargo/env
 	@cargo bench
 
 cargo-test-benches:### 	cargo-test-benches
-	cargo test --benches
+	@cargo test --benches
 
 cargo-report:### 	cargo-report
 	@. $(HOME)/.cargo/env && cargo report future-incompatibilities --id 1
