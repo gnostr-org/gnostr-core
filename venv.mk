6221002e9007b2a3e93b732fdbee3341a41e856d
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-05-01 13:51:47 -0400

    venv.mk:use pipx

--- venv.mk
+++ venv.mk
@@ -34,7 +34,8 @@ venv:
 	@#rm -rf .venv
 	@#python -c 'import sys; print (sys.real_prefix)' 2>/dev/null && INVENV=1 && echo $(INVENV) || INVENV=0 && echo $(INVENV)
 	touch requirements.txt
-	test -d .venv || $(shell which python3) -m virtualenv .venv
+	type -P pipx && pipx install virtualenv
+	test -d .venv || virtualenv .venv
 	( \
 	   source .venv/bin/activate; pip install -r requirements.txt; \
 	);
