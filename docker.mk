33f542f408968fac48403d2a9a3e83326d533646
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-05-12 20:41:22 -0400

    make:docker-build-debian

--- docker.mk
+++ docker.mk
@@ -11,3 +11,29 @@ docker-buildx:## 		docker buildx build sequence
 	@$(DOCKER) buildx create --use --name gnostr-buildx || true
 	@$(DOCKER) buildx build -t miniscript --platform linux/arm64,linux/amd64 .
 	@$(DOCKER) buildx build -t miniscript --platform linux/$(TARGET) . --load
+docker:## 	docker commands
+#                          docker                    docker
+	@awk 'BEGIN {FS = ":.*?######	"} /^[a-zA-Z_-]+:.*?######	/ {printf "\033[36m%-15s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+
+.ONESHELL:
+docker-start:###### 	detect whether docker is running...
+	@( \
+	    while ! docker system info > /dev/null 2>&1; do\
+	    echo 'Waiting for docker to start...';\
+	    if [[ '$(OS)' == 'Linux' ]]; then\
+	     systemctl restart docker.service;\
+	    fi;\
+	    if [[ '$(OS)' == 'Darwin' ]]; then\
+	     open --background -a /./Applications/Docker.app/Contents/MacOS/Docker;\
+	    fi;\
+	sleep 1;\
+	done\
+	)
+docker-pull:docker-start###### 	pull alpine image
+	docker pull alpine
+
+docker-build-debian:###### 	docker build -f ./packaging/debian/Dockerfile .
+	docker buildx build --progress=plain -f gui/packaging/debian/Dockerfile .
+	docker build -v -f gui/packaging/debian/Dockerfile .
+# vim: set noexpandtab:
+# vim: set setfiletype make
