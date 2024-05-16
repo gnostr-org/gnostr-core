0d22ccd0fc012a8e4273c06db3b8b5bf346a66f1
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-05-15 22:01:43 -0400

    make:cargo-publish-all
    naively attempt to publish everything

--- cargo.mk
+++ cargo.mk
@@ -10,6 +10,9 @@ cargo-release-all:### 	cargo-release-all
 cargo-clean-all:### 	cargo-clean-all - clean release artifacts
 ## 	cargo-clean-all 	recursively cargo clean --release
 	for t in */Cargo.toml;  do echo $$t; cargo clean --release -vv --manifest-path $$t; done
+cargo-publish-all:### 	cargo-publish-all
+## 	cargo-clean-all 	recursively publish rust projects
+	for t in */Cargo.toml;  do echo $$t; cargo publish -vv --manifest-path $$t; done
 
 cargo-install-bins:### 	cargo-install-bins
 ## 	cargo-install-all 	recursively cargo install -vv $(SUBMODULES)
