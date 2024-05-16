f85ea0142afdb120153db6944712ee7db120375e
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-05-15 23:43:28 -0400

    ..

--- go.mk
+++ go.mk
@@ -3,4 +3,5 @@ gnostr-go-chat:go-chat## go
 go:go-chat
 go-chat:
 	cd go && make &&  go build -v -o /usr/local/bin/gnostr-go-chat && cd ..
+	cd go && make &&  go build -v -o gnostr-go-chat-test-0.0.0-linux-amd64.tar.gz && cd ..
 	cp ./go/gnostr-go-chat .
