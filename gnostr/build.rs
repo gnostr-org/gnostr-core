4a186b2447fcdf0f419b798264fdbafa9f19c2ed
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-05-23 09:00:40 -0400

    gnostr/build.rs:build everytime cargo build is invoked

--- gnostr/build.rs
+++ gnostr/build.rs
@@ -1,18 +1,51 @@
-use std::process::Command;
-use std::{env, fs};
-
-use include_dir::{include_dir, Dir};
-//use std::path::Path;
-use markdown::to_html;
-
 //static PROJECT_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR");
+use std::env;
+use std::path::Path;
+use std::process::Command; //, fs};
+
+//use which::which;
+//use include_dir::{include_dir};//, Dir};
+//use std::path::Path;
+//use markdown::to_html;
 
 fn main() -> std::io::Result<()> {
     let _out_dir = env::var("OUT_DIR").unwrap();
 
+    if Path::new("nostril.c").exists() {
+        println!("cargo:rerun-if-changed=nostril.c");
+    }
+    if !Path::new("nostril").exists() {
+        println!("cargo:rerun-if-changed=nostril");
+    }
+    if Path::new("nostril").exists() {
+        println!("cargo:rerun-if-changed=nostril");
+    }
+    if !Path::new("/usr/local/bin/nostril").exists() {
+        println!("cargo:rerun-if-changed=nostril");
+    }
+    if Path::new("/usr/local/bin/nostril").exists() {
+        println!("cargo:rerun-if-changed=nostril");
+    }
+
+    if Path::new(".git/HEAD").exists() {
+        println!("cargo:rerun-if-changed=.git/HEAD");
+    }
+    if Path::new("../.git/HEAD").exists() {
+        println!("cargo:rerun-if-changed=../.git/HEAD");
+    }
+
+    // let library_path = Path::new("libsecp256k1.a");
+    // cc::Build::new()
+    //
+    //     .file("nostril.c")
+    //     .include(library_path)
+    //     .include("deps/secp256k1/include/secp256k1.h")
+    //     .compile("nostril");
+
     Command::new("git")
         .args(&["submodule", "update", "--init", "--recursive"])
-        .status()
+        .spawn()
+        //.status()
         .unwrap();
     Command::new("git")
         .args(&[
@@ -21,7 +54,8 @@ fn main() -> std::io::Result<()> {
             "randymcmillan/nostril",
             "git@github.com:randymcmillan/nostril.git",
         ])
-        .status()
+        .spawn()
+        //.status()
         .unwrap();
     Command::new("git")
         .args(&[
@@ -30,24 +64,21 @@ fn main() -> std::io::Result<()> {
             "jb55/nostril",
             "git@github.com:jb55/nostril.git",
         ])
-        .status()
+        .spawn()
+        //.status()
         .unwrap();
     Command::new("git")
         .args(&["fetch", "--all"])
-        .status()
+        .spawn()
+        //.status()
         .unwrap();
     Command::new("git")
         .args(&["fetch", "--all", "--tags", "--force"])
-        .status()
+        .spawn()
+        //.status()
         .unwrap();
-    Command::new("cmake")
-        .args(&["`pwd`"])
-        .status()
-        .unwrap();
-    Command::new("make")
-        .args(&["&"])
-        .status()
-        .unwrap();
+    Command::new("cmake").args(&["`pwd`"]).status().unwrap();
+    Command::new("make").args(&["&"]).status().unwrap();
     //Command::new("make")
     //    .args(&["nostril"])
     //    .status()
