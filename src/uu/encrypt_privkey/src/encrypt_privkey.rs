d6efb4c8f4d00bfb6d2d4c41531e30860df6ee7e
randymcmillan <randymcmillan@protonmail.com>
2024-07-27 09:10:08 -0400

    src/uu/encrypt_privkey/src/encrypt_privkey.rs:
    some code clean up

--- src/uu/encrypt_privkey/src/encrypt_privkey.rs
+++ src/uu/encrypt_privkey/src/encrypt_privkey.rs
@@ -1,4 +1,4 @@
-// This file is part of the uutils coreutils package.
+// This file is part of the gnostr-core package.
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
@@ -6,11 +6,8 @@
 // spell-checker:ignore (ToDO) fullname
 
 use clap::{crate_version, Arg, ArgAction, Command};
-use std::path::{is_separator, PathBuf};
-use uucore::display::Quotable;
-use uucore::error::{UResult, UUsageError};
-use uucore::line_ending::LineEnding;
 use uucore::{format_usage, help_about, help_usage};
+use uucore::error::UResult;
 
 static ABOUT: &str = help_about!("encrypt_privkey.md");
 
@@ -25,58 +22,11 @@ pub mod options {
 
 #[uucore::main]
 pub fn uumain(args: impl uucore::Args) -> UResult<()> {
-    let args = args.collect_lossy();
-
-    //
-    // Argument parsing
-    //
-    //let matches = uu_app().try_get_matches_from(args)?;
-
-    //let line_ending = LineEnding::from_zero_flag(matches.get_flag(options::ZERO));
-
-    ////let mut name_args = matches
-    //  //  .get_one::<String>(options::NAME)
-    //    //.unwrap();
-    //    //.collect::<Vec<_>>();
-    //let mut name_args: std::string::String = matches
-    //    .get_one::<String>(options::NAME).expect("REASON")
-    //    .to_string();
-    // if name_args.is_empty() {
-    //     //return Err(UUsageError::new(1, "missing operand".to_string()));
-    // }
-    //let multiple_paths =
-    //    matches.get_one::<String>(options::SUFFIX).is_some() || matches.get_flag(options::MULTIPLE);
-    //let suffix = if multiple_paths {
-    //    matches
-    //        .get_one::<String>(options::SUFFIX)
-    //        .cloned()
-    //        .unwrap_or_default()
-    // } else {
-    //     // "simple format"
-    //    match name_args.len() {
-    //        //0 => panic!("already checked"),
-    //        //1 => String::default(),
-    //        1 => name_args.pop().unwrap().to_string(),
-    //        _ => {
-    //            return Err(UUsageError::new(
-    //                1,
-    //                format!("extra operand {}", name_args.quote(),),
-    //            ));
-    //        }
-    //    }
-    //};
-
-    //
-    // Main Program Processing
-    //
- 
+    let _args = args.collect_lossy();
     let name_args =  String::from("");
     let suffix =  String::from("");
     let line_ending: bool = false;
-    //for path in name_args {
-        print!("\n{}\n{}\n", encrypt_privkey(&name_args, &suffix), line_ending);
-   // }
-
+    print!("\n{}\n{}\n", encrypt_privkey(&name_args, &suffix), line_ending);
     Ok(())
 }
 
@@ -118,47 +68,13 @@ pub fn uu_app() -> Command {
                 .overrides_with(options::ZERO),
         )
 }
-
-
-
-
-// TEMPORARILY
-#[allow(clippy::uninlined_format_args)]
+//#[allow(clippy::uninlined_format_args)]
 
 use gnostr_types::PrivateKey;
 use zeroize::Zeroize;
 
 // Turn a hex private key into an encrypted private key
-fn encrypt_privkey(fullname: &str, suffix: &str) -> String {
-    // if cfg!(debug_assertions) {
-    //     println!("WARNING: This takes a long time in debug mode.");
-    // }
-
-    // // Remove all platform-specific path separators from the end.
-    // let path = fullname.trim_end_matches(is_separator);
-
-    // // If the path contained *only* suffix characters (for example, if
-    // // `fullname` were "///" and `suffix` were "/"), then `path` would
-    // // be left with the empty string. In that case, we set `path` to be
-    // // the original `fullname` to avoid returning the empty path.
-    // let path = if path.is_empty() { fullname } else { path };
-
-    // // Convert to path buffer and get last path component
-    // let pb = PathBuf::from(path);
-    // match pb.components().last() {
-    //     Some(c) => {
-    //         let name = c.as_os_str().to_str().unwrap();
-    //         if name == suffix {
-    //             //name.to_string()
-    //             return name.to_string();
-    //         } else {
-    //             //name.strip_suffix(suffix).unwrap_or(name).to_string()
-    //             return name.strip_suffix(suffix).unwrap_or(name).to_string();
-    //         }
-    //     }
-
-    //     None => String::new(),
-    // };
+fn encrypt_privkey(_fullname: &str, _suffix: &str) -> String {
 
     let private_key_str = rpassword::prompt_password("Private Key (hex or bech32): ").unwrap();
 
@@ -170,7 +86,7 @@ fn encrypt_privkey(fullname: &str, suffix: &str) -> String {
         },
     };
 
-    println!("Enter the logN rounds (a power of 2, e.g. 20): ");
+    print!("Enter the logN rounds (a power of 2, default 0): ");
     let mut log_n = String::from("0");
     let stdin = std::io::stdin();
     stdin.read_line(&mut log_n).unwrap();
@@ -182,7 +98,6 @@ fn encrypt_privkey(fullname: &str, suffix: &str) -> String {
     let encrypted_private_key = private_key
         .export_encrypted(&password, log_n)
         .expect("Could not export encrypted private key");
-    //print!("{}", encrypted_private_key);
     password.zeroize();
     encrypted_private_key.to_string()
 }
