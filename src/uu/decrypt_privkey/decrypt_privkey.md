38858188ca3ba2a1706f73eee82df4b93d94aa74
randymcmillan <randymcmillan@protonmail.com>
2024-09-21 20:18:06 -0400

    src/uu/decrypt_privkey:initial config

---
+++ src/uu/decrypt_privkey/decrypt_privkey.md
@@ -0,0 +1,8 @@
+# encrypt_privkey
+
+```
+encrypt_privkey
+```
+
+encrypt a nostr (sha256) private key dialogue
+gnostr-core encrypt_privkey
---
+++ src/uu/decrypt_privkey/src/decrypt_privkey.rs
@@ -0,0 +1,95 @@
+// This file is part of the gnostr-core package.
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+
+// spell-checker:ignore (ToDO) fullname
+
+use clap::{crate_version, Arg, ArgAction, Command};
+use uucore::error::UResult;
+use uucore::{format_usage, help_about, help_usage};
+
+static ABOUT: &str = help_about!("decrypt_privkey.md");
+
+const USAGE: &str = help_usage!("decrypt_privkey.md");
+
+pub mod options {
+    pub static PRIVKEY: &str = "privkey";
+    pub static MULTIPLE: &str = "multiple";
+    pub static NAME: &str = "name";
+    pub static SUFFIX: &str = "suffix";
+    pub static ZERO: &str = "zero";
+}
+
+//use gnostr_types::PrivateKey;
+//use zeroize::Zeroize;
+mod rpassword;
+//
+//// Turn a hex private key into an encrypted private key
+//#[allow(clippy::uninlined_format_args)]
+//fn encrypt_privkey(privkey: String) -> String {
+//    let private_key_str = rpassword::prompt_password("Private Key (hex or bech32): ").unwrap();
+//
+//    let private_key = match PrivateKey::try_from_hex_string(&private_key_str) {
+//        Ok(pk) => pk,
+//        Err(_) => match PrivateKey::try_from_bech32_string(&private_key_str) {
+//            Ok(pk) => pk,
+//            Err(_) => panic!("Did not recognize private key"),
+//        },
+//    };
+//
+//    let mut log_n = rpassword::prompt_password("Enter the log_n rounds (default 0): ").unwrap();
+//    if log_n.len() == 0 {
+//        log_n = String::from("0");
+//    }
+//    log_n = log_n.trim().to_string();
+//    let log_n = log_n.parse::<u8>().unwrap();
+//    let mut password = rpassword::prompt_password("Password (default empty): ").unwrap();
+//    let encrypted_private_key = private_key
+//        .export_encrypted(&password, log_n)
+//        .expect("Could not export encrypted private key");
+//    password.zeroize();
+//    encrypted_private_key.to_string()
+//}
+
+// TEMPORARILY
+//#![allow(clippy::uninlined_format_args)]
+
+use gnostr_types::{EncryptedPrivateKey, PrivateKey};
+
+fn decrypt_privkey() {
+    println!("DANGER this exposes the private key.");
+    println!("encrypted private key: ");
+    let mut epk = String::new();
+    let stdin = std::io::stdin();
+    stdin.read_line(&mut epk).unwrap();
+    let epk = EncryptedPrivateKey(epk.trim().to_owned());
+
+    let password = rpassword::prompt_password("Password: ").unwrap();
+    let mut private_key = PrivateKey::import_encrypted(&epk, &password)
+        .expect("Could not import encrypted private key");
+    println!("{}", private_key.as_hex_string());
+}
+
+#[uucore::main]
+pub fn uumain(args: impl uucore::Args) -> UResult<()> {
+    let _args = args.collect_lossy();
+    decrypt_privkey();
+    Ok(())
+}
+
+pub fn uu_app() -> Command {
+    Command::new(uucore::util_name())
+        .version(crate_version!())
+        .about(ABOUT)
+        .override_usage(format_usage(USAGE))
+        .infer_long_args(true)
+        .arg(
+            Arg::new(options::PRIVKEY)
+                //.short('')
+                .long(options::PRIVKEY)
+                .help("decrypt privkey")
+                .action(ArgAction::SetTrue),
+            //.overrides_with(options::MULTIPLE),
+        )
+}
